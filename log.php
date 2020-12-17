136f91b645123347264dfe15a51c74e26543de38
Yurii Antonchyk <yurii.antonchyk@onthe.io>
2020-12-17 11:22:20 +0200

    Fixed show log endpoint

--- log.php
+++ log.php
@@ -3,6 +3,7 @@
 $file_name = __DIR__ . '/var/log.txt';
 
 if (is_file($file_name)) {
+    header('Content-Type: text/plain');
     http_response_code(200);
     echo file_get_contents($file_name), PHP_EOL;
 } else {
